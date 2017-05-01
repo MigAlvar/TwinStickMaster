c569a34598ca4729a70ed6e1329a62ac12a394a7
Me <migjig.20@gmail.com>
2017-05-01 08:02:10 -0400

    Initiating project using CrossplatformInput Controller

---
+++ Assets/01_Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (CrossPlatformInputManager.GetButton ("Horizontal")) {
+			print ("Horizontal");
+		}
+		if (CrossPlatformInputManager.GetButton ("Vertical")) {
+			print("Vertical");
+		}
+	}
+}
